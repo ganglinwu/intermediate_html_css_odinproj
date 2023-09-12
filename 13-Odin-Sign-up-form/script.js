d9c7ec659ef769ebc33ac7653ba6d0027c5a8939
ganglinwu <ganglin.wu@gmail.com>
2023-09-12 16:03:51 +0800

    Initial commit
    - skeletal index.html, style.css, script.js

---
+++ 13-Odin-Sign-up-form/script.js
@@ -0,0 +1,25 @@
+const pw1 = document.querySelector('')
+const pw2 = document.querySelector('')
+const form = document.querySelector('form')
+
+
+function passwordMatch() {
+    if (pw1.textContent == pw2.textContent) {
+        return true
+    } else {
+        return false
+    }
+}
+
+form.addEventListener('submit' (e)= {
+    
+    if () {
+        // if form is valid
+
+    } else {
+        // if form is invalid
+        e.preventDefault();
+    }
+
+
+})
