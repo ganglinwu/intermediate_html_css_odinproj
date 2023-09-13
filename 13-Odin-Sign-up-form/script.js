eac185f2733cccc244019e0e3be3d5f201970fa8
ganglinwu <ganglin.wu@gmail.com>
2023-09-13 13:24:39 +0800

    Test form completed

--- 13-Odin-Sign-up-form/script.js
+++ 13-Odin-Sign-up-form/script.js
@@ -3,23 +3,51 @@ const pw2 = document.querySelector('#pw2')
 const form = document.querySelector('form')
 
 
+
 function passwordMatch() {
-    if (pw1.textContent == pw2.textContent) {
+    if (pw1.value == pw2.value) {
         return true
     } else {
         return false
     }
 }
 
-form.addEventListener('submit' (e)= {
-    
-    if () {
-        // if form is valid
+form.addEventListener('submit', validateForm)
 
+function validateForm(e) {
+    const form = e.target;
+
+    if (form.checkValidity()) {
+        Array.from(form.elements).forEach(i=> {
+            i.parentElement.classList.remove('invalid');
+        })
     } else {
-        // if form is invalid
         e.preventDefault();
+        
+        Array.from(form.elements).forEach(i=> {
+            if (i.checkValidity()) {
+                i.parentElement.classList.remove('invalid');
+            } else {
+                i.parentElement.classList.add('invalid');
+            }
+        });
+    } 
+    if (!passwordMatch()) {
+        pw1.parentElement.classList.add('invalid');
+        pw2.parentElement.classList.add('invalid');
+        e.preventDefault();
+    } else {
+        pw1.parentElement.classList.remove('invalid');
+        pw2.parentElement.classList.remove('invalid');
     }
+}
 
-
+Array.from(form.elements).forEach(i=> {
+    i.addEventListener('input', (e)=> {
+        if (i.checkValidity()) {
+            i.classList.remove('incorrect');
+        } else {
+            i.classList.add('incorrect');
+        }
+    })
 })
