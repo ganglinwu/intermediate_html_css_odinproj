50e262a5a4c47a26624adcca8b3225e45fdcb0fc
ganglinwu <ganglin.wu@gmail.com>
2023-09-13 14:06:13 +0800

    Allow form to show a pop up when form is correctly entered
    - changed eventlistener to listen for button click instead of form submit
    - form will only be considered correct if
        - forms are filled correctly
        - password is not blank
        - passwords match

--- 13-Odin-Sign-up-form/script.js
+++ 13-Odin-Sign-up-form/script.js
@@ -1,6 +1,7 @@
 const pw1 = document.querySelector('#pw1')
 const pw2 = document.querySelector('#pw2')
 const form = document.querySelector('form')
+const button = document.querySelector('button')
 
 
 
@@ -12,17 +13,35 @@ function passwordMatch() {
     }
 }
 
-form.addEventListener('submit', validateForm)
+button.addEventListener('click', validateForm)
 
 function validateForm(e) {
-    const form = e.target;
+    const form = e.target.form;
+    let displayAlert = false;
 
     if (form.checkValidity()) {
         Array.from(form.elements).forEach(i=> {
             i.parentElement.classList.remove('invalid');
         })
+        if (!pw1.value) {
+            pw1.parentElement.classList.add('empty');
+        } else {
+            pw1.parentElement.classList.remove('empty');
+            
+            if (!passwordMatch()) {
+                pw1.parentElement.classList.add('invalid');
+                pw2.parentElement.classList.add('invalid');
+                e.preventDefault();
+                displayAlert = false;
+            } else {
+                pw1.parentElement.classList.remove('invalid');
+                pw2.parentElement.classList.remove('invalid');
+                displayAlert = true;
+            }
+        }
     } else {
         e.preventDefault();
+        displayAlert = false;
         
         Array.from(form.elements).forEach(i=> {
             if (i.checkValidity()) {
@@ -32,14 +51,7 @@ function validateForm(e) {
             }
         });
     } 
-    if (!passwordMatch()) {
-        pw1.parentElement.classList.add('invalid');
-        pw2.parentElement.classList.add('invalid');
-        e.preventDefault();
-    } else {
-        pw1.parentElement.classList.remove('invalid');
-        pw2.parentElement.classList.remove('invalid');
-    }
+    (displayAlert) ? alert('Form looks good! It has been \'submitted\'') : e.preventDefault();
 }
 
 Array.from(form.elements).forEach(i=> {
